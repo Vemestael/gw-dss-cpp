c7d8802ebedfe01958083a3baa8303a9fccbe38f
Vladimir Grigoryev <volodimir.grigoryev@gmail.com>
2021-04-24 14:57:42 +0300

    add Qt resource files & add nord theme

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file(":/styles/nord.qss");
+    file.open(QFile::ReadOnly);
+    QString style = QLatin1String(file.readAll());
+    qApp->setStyleSheet(style);
+
     MainWindow w;
     w.show();
     return a.exec();
