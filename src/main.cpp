b29b2effbc4ea6894b261c88a3b0b78462b0071f
Mykola Symon <simonnikolaj20@gmail.com>
2021-04-28 15:22:20 +0300

    add app ico (#4)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/icon-64.png"));
 
     QFile file(":/styles/nord.qss");
     file.open(QFile::ReadOnly);
