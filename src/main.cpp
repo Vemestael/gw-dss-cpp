9c5cabac7da444f85efe9ef609af632e16969f0a
Mykola Symon <simonnikolaj20@gmail.com>
2021-04-28 15:21:22 +0300

    hotfix

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/icon-64.png"));
 
     QFile file(":/styles/nord.qss");
     file.open(QFile::ReadOnly);
     QString style = QLatin1String(file.readAll());
     qApp->setStyleSheet(style);
-
-    a.setWindowIcon(QIcon(":/icons/icon-64.png"));
 
     MainWindow w;
     w.show();
