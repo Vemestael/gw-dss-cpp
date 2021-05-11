2cab921d9fd68815285646c411d66ed82d52e4dd
Vladimir Grigoryev <volodimir.grigoryev@gmail.com>
2021-05-11 13:57:46 +0300

    remove debug console

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/icons/icon-64.png"));
 
-    // OUTPUT DEBUG INFO
-    // REMOVE
-    AllocConsole();
-    freopen("CONOUT$", "w", stdout);
-    freopen("CONOUT$", "w", stderr);
-
     QFile file(":/styles/nord.qss");
     file.open(QFile::ReadOnly);
     QString style = QLatin1String(file.readAll());
